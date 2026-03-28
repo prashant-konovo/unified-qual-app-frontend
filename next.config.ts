f64c4daddd4c744cef7ba5c75a8790e13e317281
prashant-konovo-wsl <prashant.singh@konovo.com>
2026-03-28 23:53:31 +0530

    fix: update NLB hostname after ArgoCD recreated LoadBalancer service

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/backend/:path*",
-        destination: `${process.env.BACKEND_URL ?? "http://a6e1211d45bda4b32b51f6b2d278bce7-2c5abb46dc88fb21.elb.us-east-2.amazonaws.com"}/v1/:path*`,
+        destination: `${process.env.BACKEND_URL ?? "http://a6e1211d45bda4b32b51f6b2d278bce7-1983641930.us-east-2.elb.amazonaws.com"}/v1/:path*`,
       },
     ];
   },
